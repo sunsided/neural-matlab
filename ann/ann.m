14a02011627227b4b3e227214b5956c7924ad534
Markus Mayer <widemeadows@gmail.com>
2016-08-22 20:44:00 +0200

    added network execution

--- ann/ann.m
+++ ann/ann.m
@@ -4,42 +4,71 @@
 rng(42);
 
 % Learning XOR
+% ------------
 
-X = { [0 0]; 
-      [1 0]; 
-      [0 1]; 
-      [1 1] };
+X = { [0; 0]; 
+      [1; 0]; 
+      [0; 1]; 
+      [1; 1] };
   
 Y = { 0;
       1;
       1;
       0 };
 
+assert( numel(X) == numel(Y) );
+  
 % The "input layer"
+% -----------------
+
 % This layer does not exist, since the "outgoing" connection weights
 % are encoded as part of the first hidden layer and the "input neurons"
 % just act as identities.
   
-% Defining the hidden layer
+% The (first and only) hidden layer
+% ---------------------------------
 
 N_inputs     = numel(X{1});
 N_neurons    = 2;
-activation   = @(z) 1/(1+exp(-z));
-d_activation = @(z) activation(z)*(1-activation(z));
+activation   = @(z) 1./(1+exp(-z)); % logistic function
+d_activation = @(z) activation(z).*(1-activation(z));
 L{1}         = struct(...
                 'theta',  randn(N_neurons, N_inputs), ...
+                'bias',   randn(N_neurons, 1), ...
                 'sigma',  activation, ...
                 'dsigma', d_activation ...
                 );
 
-% Defining the output layer
+% The output layer
+% ----------------
 
-N_inputs     = size(L{1}.theta, 1);
+N_inputs     = size(L{1}.theta, 1); % no. inputs is no. previous outputs
 N_neurons    = 1;
-activation   = @(z) 1/(1+exp(-z));
-d_activation = @(z) activation(z)*(1-activation(z));
+activation   = L{1}.sigma;          % using the same activation function
+d_activation = L{1}.dsigma;
 L{2}         = struct(...
                 'theta',  randn(N_neurons, N_inputs), ...
+                'bias',   randn(N_neurons, 1), ...
                 'sigma',  activation, ...
                 'dsigma', d_activation ...
                 );
+            
+% Test execution of the network
+% -----------------------------
+
+disp('Untrained network:');
+
+for i=1:numel(X)     % ... for each training sample ...
+    a = X{i};        % The first layer's activation is the network input.
+
+    for j=1:numel(L) % ... for each network layer sample ...
+        weights = L{j}.theta;
+        bias    = L{j}.bias;
+        sigma   = L{j}.sigma;
+        
+        z = weights*a + bias;
+        a = sigma(z);
+    end
+    
+    disp([ '  h( ' mat2str(X{i}) ' ) = ' mat2str(a) ]);
+end
